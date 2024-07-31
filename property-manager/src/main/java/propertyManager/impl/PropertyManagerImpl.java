00a0a3b4282cfd21c96b19effb9e6dbc9eafbab7
최철진 <cil6879@gmail.com>
2024-07-31 15:41:52 +0900

    Revert "deleteProperty 메소드 완성"
    This reverts commit 01f790a67e369d292c30d03f60a0d5b627924251.

--- property-manager/src/main/java/propertyManager/impl/PropertyManagerImpl.java
+++ property-manager/src/main/java/propertyManager/impl/PropertyManagerImpl.java
@@ -33,41 +33,6 @@ public class PropertyManagerImpl implements PropertyManager {
 
     @Override
     public void deleteProperty(int property_id) {
-        Connection conn = null;
-        PreparedStatement pstmt = null;
-        try {
-            conn = DriverManager.getConnection("jdbc:mysql://59.27.84.200:3306/workshop", "grepp", "grepp");
-
-            String SQL = "DELETE FROM property_tb WHERE property_id = ?";
-
-            pstmt = conn.prepareStatement(SQL);
-            pstmt.setInt(1, property_id);
-
-            int result = pstmt.executeUpdate();
-
-            System.out.println("property_id에 해당하는 property 정보가 삭제되었습니다." + result);
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-            throw new RuntimeException(e);
-        } finally {
-            if (pstmt != null) {
-                try {
-                    pstmt.close();
-                } catch (SQLException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-
-            if (conn != null) {
-                try {
-                    conn.close();
-                } catch (SQLException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-        }
-
 
     }
 }
