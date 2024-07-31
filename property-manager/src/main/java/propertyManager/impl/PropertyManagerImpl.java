1ba5daf90eeb0a437f0af96fdc17701a48890694
최철진 <cil6879@gmail.com>
2024-07-31 16:34:02 +0900

    deleteProperty 메소드 완성 2

--- property-manager/src/main/java/propertyManager/impl/PropertyManagerImpl.java
+++ property-manager/src/main/java/propertyManager/impl/PropertyManagerImpl.java
@@ -6,6 +6,13 @@ import propertyManager.PropertyManager;
 import java.util.List;
 
 public class PropertyManagerImpl implements PropertyManager {
+
+    Connection conn;
+
+    public PropertyManagerImpl(Connection conn) {
+        this.conn = conn;
+    }
+
     @Override
     public void addProperty(Property p) {
 
@@ -33,38 +40,15 @@ public class PropertyManagerImpl implements PropertyManager {
 
     @Override
     public void deleteProperty(int property_id) {
-        Connection conn = null;
-        PreparedStatement pstmt = null;
-        try {
-            conn = DriverManager.getConnection("jdbc:mysql://59.27.84.200:3306/workshop", "grepp", "grepp");
-            String SQL = "DELETE FROM property WHERE property_id = ?";
-
-            pstmt = conn.prepareStatement(SQL);
+        String SQL = "DELETE FROM property_tb WHERE property_id = ?";
+        try (PreparedStatement pstmt = conn.prepareStatement(SQL)) {
             pstmt.setInt(1, property_id);
-
             int result = pstmt.executeUpdate();
-
-            System.out.println("property_id에 해당하는 property가 삭제되었습니다" + result);
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-            throw new RuntimeException(e);
-        } finally {
-            if (pstmt != null) {
-                try {
-                    pstmt.close();
-                } catch (SQLException e) {
-                    throw new RuntimeException(e);
-                }
+            System.out.println("property_id에 해당하는 property가 삭제되었습니다 :”
+                + result); } catch(SQLException e) {
+                e.printStackTrace();
+                throw new RuntimeException(e);
             }
 
-            if (conn != null) {
-                try {
-                    conn.close();
-                } catch (SQLException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-        }
     }
 }
