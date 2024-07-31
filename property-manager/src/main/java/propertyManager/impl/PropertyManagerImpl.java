5f2880c06078266fcf696acb65134dcb4238a7b6
dowoo kim <dowoo12345@naver.com>
2024-07-31 15:24:21 +0900

    fix PropertyManagerImpl

--- property-manager/src/main/java/propertyManager/impl/PropertyManagerImpl.java
+++ property-manager/src/main/java/propertyManager/impl/PropertyManagerImpl.java
@@ -1,4 +1,38 @@
 package propertyManager.impl;
 
-public class PropertyManagerImpl {
+import entity.Property;
+import propertyManager.PropertyManager;
+
+import java.util.List;
+
+public class PropertyManagerImpl implements PropertyManager {
+    @Override
+    public void addProperty(Property p) {
+
+    }
+
+    @Override
+    public List<Property> getProperties() {
+        return null;
+    }
+
+    @Override
+    public List<Property> searchByWidth() {
+        return null;
+    }
+
+    @Override
+    public List<Property> searchBySoldDate() {
+        return null;
+    }
+
+    @Override
+    public void updateSoldDate(int propertyId, String soldDate) {
+
+    }
+
+    @Override
+    public void deleteProperty(int property_id) {
+
+    }
 }
