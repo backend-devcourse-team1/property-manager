1aec2fedc600a8342c857d60081ab424e292b5f2
dowoo kim <dowoo12345@naver.com>
2024-07-31 16:05:53 +0900

    feat: PropertyManagerImpl getProperties

--- property-manager/src/main/java/propertyManager/impl/PropertyManagerImpl.java
+++ property-manager/src/main/java/propertyManager/impl/PropertyManagerImpl.java
@@ -1,11 +1,24 @@
 package propertyManager.impl;
 
 import entity.Property;
+import io.github.cdimascio.dotenv.Dotenv;
 import propertyManager.PropertyManager;
 
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class PropertyManagerImpl implements PropertyManager {
+    private static final Dotenv dotenv = Dotenv.load();
+
+    private static final String URL = dotenv.get("DB_URL");
+    private static final String USER = dotenv.get("DB_USER");
+    private static final String PASSWORD = dotenv.get("DB_PASSWORD");
+
+    private Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+
     @Override
     public void addProperty(Property p) {
 
@@ -13,7 +26,23 @@ public class PropertyManagerImpl implements PropertyManager {
 
     @Override
     public List<Property> getProperties() {
-        return null;
+        List<Property> properties = new ArrayList<>();
+        String sql = "SELECT * FROM property";
+        try (Connection conn = getConnection(); Statement stmt = conn.createStatement(); ResultSet rs = stmt.executeQuery(sql)) {
+            while (rs.next()) {
+                Property property = new Property();
+                property.setProperty_id(rs.getInt("property_id"));
+                property.setParticipant_id(rs.getInt("participant_id"));
+                property.setUp_date(rs.getDate("up_date"));
+                property.setSold_date(rs.getDate("sold_date"));
+                property.setAddress(rs.getString("address"));
+                property.setWidth(rs.getInt("width"));
+                properties.add(property);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return properties;
     }
 
     @Override
