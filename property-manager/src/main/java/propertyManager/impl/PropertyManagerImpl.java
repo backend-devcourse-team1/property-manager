d957dd184534aa5f7bdf3f546c495334d14e3547
LeeJongWon <106072775+LJW22222@users.noreply.github.com>
2024-07-31 15:42:36 +0900

    update

--- property-manager/src/main/java/propertyManager/impl/PropertyManagerImpl.java
+++ property-manager/src/main/java/propertyManager/impl/PropertyManagerImpl.java
@@ -2,6 +2,7 @@ package propertyManager.impl;
 
 import entity.Property;
 import propertyManager.PropertyManager;
+import java.sql.*;
 
 import java.util.List;
 
@@ -23,9 +24,34 @@ public class PropertyManagerImpl implements PropertyManager {
 
     @Override
     public List<Property> searchBySoldDate() {
-        return null;
+        sql = "select * from property order by sold_date desc ";
+        List<Property> resultList = new ArrayList<>();
+        try {
+            conn = getConnection();
+            pstmt = conn.prepareStatement(sql);
+            ResultSet rs = pstmt.executeQuery();
+            while (rs.next()) {
+                Property property = new Property(
+                        rs.getString("participant_id"),
+                        rs.getDate("up_date"),
+                        rs.getDate("sold_date"),
+                        rs.getString("address"),
+                        rs.getInt("width")
+                );
+                resultList.add(property);
+            }
+        }catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                if (pstmt != null) pstmt.close();
+                if (conn != null) conn.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return resultList;
     }
-
     @Override
     public void updateSoldDate(int propertyId, String soldDate) {
 
